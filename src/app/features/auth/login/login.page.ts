159d46b5ba1d4103501f546840a1989b0ad439f7
Pedro Carmona <pedrocarmona@MacBook-Pro-de-Pedro.local>
2024-10-11 22:33:13 -0500

    add services

--- src/app/features/auth/login/login.page.ts
+++ src/app/features/auth/login/login.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon } from '@ionic/angular/standalone';
+import { LoginService } from 'src/app/core/services/auth/login.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -12,8 +15,37 @@ import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon } from '@ionic/ang
 })
 export class LoginPage {
 
-  constructor() { }
+  isPasswordVisible: boolean = false;
+  username: string= '' ;
+  password: string = '';
 
-  
+  constructor(
+    private loginService:LoginService,
+    private router:Router
+  ) { }
 
+  togglePasswordVisibility(): void {
+    this.isPasswordVisible = !this.isPasswordVisible;
+  }
+
+  get isPasswordValid(): boolean {
+    return this.password.length >= 6;
+  }
+
+  get isFormValid(): boolean {
+    return this.isPasswordValid;
+  }
+
+  login() {
+    this.loginService.login(this.username, this.password)
+      .then(response => {
+        console.log('Respuesta del servicio:', response);
+        this.router.navigate(['home'])
+        // Aquí puedes manejar la respuesta, como guardar el token
+      })
+      .catch(error => {
+        console.error('Error al iniciar sesión:', error);
+        // Aquí puedes manejar el error, como mostrar un mensaje al usuario
+      });
+  }
 }
---
+++ src/app/models/login.model.ts
@@ -0,0 +1,7 @@
+// src/app/models/login-response.model.ts
+export interface LoginResponse {
+    ok: string;
+    token: string;
+    message: string;
+  }
+  
