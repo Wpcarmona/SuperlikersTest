4460ea95dda7a1069b8cc799a895cdf6eb5ece37
Pedro Carmona <pedrocarmona@MacBook-Pro-de-Pedro.local>
2024-10-14 20:47:20 -0500

    add unitest

--- src/app/core/services/auth/login.service.spec.ts
+++ src/app/core/services/auth/login.service.spec.ts
@@ -1,16 +1,75 @@
 import { TestBed } from '@angular/core/testing';
-
 import { LoginService } from './login.service';
+import { DUMMY_LOGIN_ERROR_RESPONSE, DUMMY_LOGIN_RESPONSE } from 'src/utils/dummys/auth.dummy';
+import axios from 'axios';
+import { LoginResponse } from 'src/app/models/login.model';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('LoginService', () => {
   let service: LoginService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [LoginService]
+    });
     service = TestBed.inject(LoginService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return dummy login response when isDummy is true', (done) => {
+    service.login('123', 'password', true).subscribe(response => {
+      expect(response).toEqual(DUMMY_LOGIN_RESPONSE);
+      done();
+    });
+  });
+
+  it('should return dummy error response when isDummy and simulateError are true', (done) => {
+    service.login('123', 'password', true, true).subscribe({
+      error: (error) => {
+        expect(error.status).toBe(404);
+        expect(error.error).toEqual(DUMMY_LOGIN_ERROR_RESPONSE);
+        done();
+      }
+    });
+  });
+
+  it('should perform real login request and return response', (done) => {
+    const mockResponse: LoginResponse = {
+      token: '12345',
+      ok: 'true',
+      message:'prueba exitosa'
+    };
+
+    mockedAxios.post.mockResolvedValue({ data: mockResponse });
+
+    service.login('12222222', '123456789').subscribe(response => {
+      expect(response).toEqual(mockResponse);
+      done();
+    });
+
+    expect(mockedAxios.post).toHaveBeenCalledWith(service['apiUrl'], {
+      campaign: '4u',
+      participation: {
+        'codigo-de-cliente': '123',
+        password: 'password',
+      },
+    });
+  });
+
+  it('should handle an error response from the server', (done) => {
+    const mockError = { response: { data: { message: 'Invalid credentials' } } };
+    mockedAxios.post.mockRejectedValue(mockError);
+
+    service.login('123', 'wrong-password').subscribe({
+      error: (error) => {
+        expect(error).toEqual(mockError.response.data);
+        done();
+      }
+    });
   });
 });
--- src/app/core/services/kpi/kpi.service.spec.ts
+++ src/app/core/services/kpi/kpi.service.spec.ts
@@ -1,16 +1,87 @@
 import { TestBed } from '@angular/core/testing';
-
 import { KpiService } from './kpi.service';
+import { of, throwError } from 'rxjs';
+import axios from 'axios';
+import { DUMMY_ENTRIES_RESPONSE } from 'src/utils/dummys/entries.dummy';
+import { EntriesResponse } from 'src/app/models/entries.model';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('KpiService', () => {
   let service: KpiService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [KpiService],
+    });
     service = TestBed.inject(KpiService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return dummy entries response when isDummy is true', async () => {
+    const result = await (await service.getEntries('123', true)).toPromise();
+    expect(result).toEqual(DUMMY_ENTRIES_RESPONSE);
+  });
+
+  it('should perform real request and return entries response', async () => {
+    const mockResponse: EntriesResponse = {
+      data: {
+        previous_page_token: 123,
+        next_page_token: 456,
+        entries: [
+          {
+            name: 'Test Entry',
+            meta_mes: 100,
+            avance_actual: 50,
+            activity_count: 0,
+            category: '',
+            created_at: '',
+            moderation: '',
+            points: 0,
+            state: '',
+            id: '',
+            slug: '',
+            participant: undefined,
+            as_form: false,
+            data: undefined
+          },
+        ],
+      },
+      ok: ''
+    }
+
+    mockedAxios.post.mockResolvedValue({ data: mockResponse });
+
+    const result = await (await service.getEntries('123', false)).toPromise();
+    expect(result).toEqual(mockResponse);
+    expect(mockedAxios.post).toHaveBeenCalledWith(
+      `${service['apiUrl']}`,
+      // expect.any(), 
+      // expect.objectContaining({
+      //   headers: { 'Content-Type': 'application/json' },
+      // })
+    );
+  });
+
+  it('should handle error from server response', async () => {
+    const mockError = {
+      response: {
+        data: {
+          message: 'Error fetching entries',
+        },
+      },
+    };
+
+    mockedAxios.post.mockRejectedValue(mockError);
+
+    try {
+      await (await service.getEntries('123', false)).toPromise();
+    } catch (error) {
+      expect(error).toEqual(mockError.response.data);
+    }
   });
 });
